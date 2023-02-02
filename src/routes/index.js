786118ae7ff111f6f288311eabcdf650694266d7
Margo <marglodistovar@gmail.com>
2023-02-01 22:22:31 -0300

    Modificación de controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuarios');
 const loginController = require('../controllers/login');
-const { showUser } = require('../services/usuarios');
+
 
 router.post('/usuarios', usuariosController.create);
 router.post('/login',loginController.login)
 
 router.get('/usuarios', usuariosController.list);
-router.get('/usuarios', usuariosController.findOne)
+router.get('/usuarios', usuariosController.showUser)
 
 
 module.exports = { router };
